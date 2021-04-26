25e5f8db85b091ae3809a3d5edf3337d6bc97298
Aleksandr <a230283@yandex.ru>
2021-04-26 19:35:18 +0400

    lesson1

--- Project1/src/main/java/ru/andreev_aleksandr/Java3/lesson4/mainApp.java
+++ Project1/src/main/java/ru/andreev_aleksandr/Java3/lesson4/mainApp.java
@@ -1,4 +1,17 @@
 package ru.andreev_aleksandr.Java3.lesson4;
 
 public class mainApp {
+
+
+    public static void main(String[] args) {
+        MonABC monABC = new MonABC();
+
+        Thread thread1 = new Thread(monABC::printA);
+        Thread thread2 = new Thread(monABC::printB);
+        Thread thread3 = new Thread(monABC::printC);
+
+        thread1.start();
+        thread2.start();
+        thread3.start();
+    }
 }
